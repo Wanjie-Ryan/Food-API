c4d299b6bd3654d881c06ba88b1cbacf9a467023
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-02 14:37:27 +0300

    created the model, moving to the repository and connecting the db, postgresql

--- src/main/java/com/foods/api/food/FoodRepository.java
+++ src/main/java/com/foods/api/food/FoodRepository.java
@@ -1,4 +1,12 @@
 package com.foods.api.food;
 
-public interface FoodRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface FoodRepository extends JpaRepository<FoodModel, UUID> {
+
 }
